0a78343a39ff04ef6aeb380a5577402250f0a3bb
Siddharthk9 <126328425+Siddharthk9@users.noreply.github.com>
2026-01-22 23:53:25 +0530

    Update demo_streamlit.py

--- Recommendation_system/demo_streamlit.py
+++ Recommendation_system/demo_streamlit.py
@@ -90,9 +90,20 @@ h2, h3 {
 # Load & preprocess data
 # -------------------------------------------------
 @st.cache_data
+# def load_data():
+#     raw = pd.read_csv("clean_data.csv")
+#     return process_data(raw)
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+@st.cache_data
 def load_data():
-    raw = pd.read_csv("clean_data.csv")
-    return process_data(raw)
+    data_path = os.path.join(BASE_DIR, "clean_data.csv")
+    raw = pd.read_csv(data_path)
+    return raw
+
 
 data = load_data()
 
